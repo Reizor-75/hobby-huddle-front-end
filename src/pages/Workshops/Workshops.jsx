fbb222a1e52f4e10d9f655bc65c7a81adb47071e
mlinares <m.linares93@gmail.com>
2024-02-11 16:08:24 -0500

    create workshop services and update workshops view

--- src/pages/Workshops/Workshops.jsx
+++ src/pages/Workshops/Workshops.jsx
@@ -1,8 +1,29 @@
+//npm modules
+import { useState, useEffect } from "react";
+
+//services
+import * as workshopService from '../../services/workshopService'
+
 const Workshops = () => {
+  const [workshops, setWorkshops] = useState([])
+
+  useEffect(() => {
+    const fetchVenues = async () => {
+      const workshopData = await workshopService.getAllWorkshops()
+      setWorkshops(workshopData)
+    }
+    fetchVenues()
+  }, [])
+
+  if(!workshops.length) return <h1>No Workshops Available</h1>
+
   return (  
-    <>
+    <main>
       <h1>Workshop</h1>
-    </>
+      {workshops.map(workshop =>(
+        <h2 key={workshop._id}>{workshop.eventType}</h2>
+      ))}
+    </main>
   );
 }
 

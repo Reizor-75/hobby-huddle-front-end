ecdc9faf353bc36751e0476c8be7f1abd9e199fd
mlinares <m.linares93@gmail.com>
2024-02-13 16:30:15 -0500

    update PosterInfo to include default images

--- src/components/PosterInfo/PosterInfo.jsx
+++ src/components/PosterInfo/PosterInfo.jsx
@@ -1,6 +1,22 @@
-const PosterInfo = ({poster}) => {
+import mentorHand from '../../assets/MentorHand.png'
+import studentHand from '../../assets/StudentHand.png'
+import vendorHand from '../../assets/VendorHand.png'
+
+import styles from './PosterInfo.module.css'
+
+const PosterInfo = ({poster}) => {  
+  const setPhoto = () => {
+    if(poster.role === 500) return mentorHand
+    if(poster.role == 200) return studentHand
+    return vendorHand
+  }
+
+  const photo = poster.photo? poster.photo:setPhoto()
+
+  console.log(photo)
   return (  
     <div className=""> 
+      <img src={photo} alt="Profile Photo" className={styles.profilephoto} />
       <div className="hosting-line">Hosted By {poster.name}</div>
     </div>
   );

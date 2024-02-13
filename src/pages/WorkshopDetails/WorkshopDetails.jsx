62adfb7a483d26edd4e6beb9a15694ae608c5f56
mlinares <m.linares93@gmail.com>
2024-02-12 21:53:53 -0500

    update WorkshopDetail restricted views

--- src/pages/WorkshopDetails/WorkshopDetails.jsx
+++ src/pages/WorkshopDetails/WorkshopDetails.jsx
@@ -7,6 +7,7 @@ import * as workshopService from '../../services/workshopService'
 
 //css
 import styles from './WorkshopDetails.module.css'
+import PostDetails from "../../components/PostDetails/PostDetails";
 
 const WorkshopDetails = ({user, handleDeleteWorkshop}) => {
   const { workshopId } = useParams()
@@ -26,21 +27,22 @@ const WorkshopDetails = ({user, handleDeleteWorkshop}) => {
 
   return (  
     <div className={styles.container}>
-      <h1 className={styles.title}> {workshop.title} </h1>
-      <h2> Hosted by {workshop.mentorInfo.name}</h2>
-      <h3 className={styles.row}>
-        {/* <div>Venue: {workshop.location.venueTitle} </div> */}
+      <PostDetails content={workshop}/>
+      <div>Venue: {workshop.location.venueTitle} </div>
+      <h5 className={styles.row}>
         <div>Price: ${workshop.pricePerPerson} </div>
-      </h3>
-      <h3 className={styles.row}>
-        <div>{workshop.date}</div>
         <div>Spots remaining: {workshop.workshopLimit - 0} </div>
-      </h3>
-      <p> {workshop.description} </p>
+      </h5>
+      {/* <p> {workshop.description} </p> */}
+      <h3> Hosted by {workshop.mentorInfo.name}</h3>
       <div id={styles.applyButton}>
       {user.profile === workshop.mentorInfo._id 
         ? <button id={styles.signUpButton} onClick={()=> handleDeleteWorkshop(workshopId)}>Delete Workshop</button>
-        : <button id={styles.signUpButton}>Apply</button>
+        : <></>
+      }
+      {user.role === 200
+        ?<button id={styles.signUpButton}>Apply</button>
+        : <></>
       }
         
       </div>

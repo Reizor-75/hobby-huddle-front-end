113dc194c022c17f4ba1fcf9e51a5c673beb6a1d
mlinares <m.linares93@gmail.com>
2024-02-13 22:01:01 -0500

    update postCard styling

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -9,17 +9,20 @@ const PostDetails = ({content}) => {
   return (  
     <div className='post-details-container'>
       <div>
-      <div className='workshop-title'>{content.title}</div>
+        <div className='workshop-title'>{content.title}</div>
+        <div className='row'>
+          <div>
+            <div className='date'>{date}</div>
+            <div className='time'>{time}</div> 
+          </div>
+          <div className='row-right'> 
+            <div className='spots'>Spots Remaining: {content.workshopLimit - content.studentsAttending.length}</div>
+            <div className='cost'>Price: ${content.pricePerPerson}</div>
+          </div>
+        </div>   
+      </div>
       <div className='row'>
-        <div>
-          <div className='date'>{date}</div>
-          <div className='time'>{time}</div> 
-        </div>
-        <div className='row-right'> 
-          <div className='spots'>Spots Remaining: {content.workshopLimit - content.studentsAttending.length}</div>
-          <div className='cost'>Price: ${content.pricePerPerson}</div>
-        </div>
-      </div>   
+        {content.description}
       </div>
       <div className='bottom-row'>
       <PosterInfo poster={content.mentorInfo}/>  

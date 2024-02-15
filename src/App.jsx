afd7e44e0881297547cae1e5ae77ceb46c201818
mlinares <m.linares93@gmail.com>
2024-02-15 11:18:38 -0500

    move request handle functions to requests view

--- src/App.jsx
+++ src/App.jsx
@@ -118,25 +118,6 @@ function App() {
     navigate('/requests')
   }
 
-  const handleDeleteRequest = async (requestId) => {
-    const deletedRequest = await requestService.deleteRequest(requestId)
-    setRequests(requests.filter(request => request._id !== deletedRequest._id))
-    navigate('/requests')
-  }
-
-  const handleAddBid = async (requestId, requestFormData) => {
-    const updatedRequest = requests.find(request => request._id === requestId)
-    const newBid = await requestService.createBid(requestId, requestFormData)
-    setRequests(requests.map((request) => updatedRequest._id === request._id ? {...updatedRequest, bids:[newBid, ...updatedRequest.bids]} : request )) 
-    navigate('/requests')
-  }
-
-  const handleDeleteBid = async (requestId, bidId) => {
-    const updatedRequest = requests.find(request => request._id === requestId)
-    await requestService.deleteBid(requestId, bidId) 
-    setRequests(requests.map((request) => updatedRequest._id === request._id ? {...requests.find(request => request._id === requestId), bids: updatedRequest.bids.filter((bid) => bid._id !== bidId)} : request ))
-    navigate(`/requests`)
-  }
 
   const handleLogout = () => {
     authService.logout()
@@ -256,13 +237,7 @@ function App() {
           path="/requests"
           element={
             <ProtectedRoute user={user}>
-              <Requests 
-                user={user} 
-                requests={requests} 
-                handleDeleteRequest={handleDeleteRequest}
-                handleAddBid={handleAddBid}
-                handleDeleteBid={handleDeleteBid}
-              />
+              <Requests user={user} state={requests} />
             </ProtectedRoute>
           }
         />
--- src/pages/Requests/Requests.jsx
+++ src/pages/Requests/Requests.jsx
@@ -1,13 +1,36 @@
 //npm modules
-import { NavLink  } from 'react-router-dom'
-
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom'
 //components
 import PostCard from "../../components/PostCard/PostCard";
 
+import * as requestService from "../../services/requestService"
 //css
 import './Requests.css'
+const Requests = ({user, state}) => {
+  const [requests, setRequests] = useState(state)
+  const navigate = useNavigate()
 
-const Requests = ({user, requests, handleDeleteRequest, handleAddBid, handleDeleteBid}) => {
+  const handleDeleteRequest = async (requestId) => {
+    const deletedRequest = await requestService.deleteRequest(requestId)
+    setRequests(requests.filter(request => request._id !== deletedRequest._id))
+    navigate('/requests')
+  }
+
+  const handleAddBid = async (requestId, requestFormData) => {
+    const updatedRequest = requests.find(request => request._id === requestId)
+    const newBid = await requestService.createBid(requestId, requestFormData)
+    setRequests(requests.map((request) => updatedRequest._id === request._id ? {...updatedRequest, bids:[newBid, ...updatedRequest.bids]} : request )) 
+    navigate('/requests')
+  }
+
+  const handleDeleteBid = async (requestId, bidId) => {
+    const updatedRequest = requests.find(request => request._id === requestId)
+    await requestService.deleteBid(requestId, bidId) 
+    setRequests(requests.map((request) => updatedRequest._id === request._id ? {...requests.find(request => request._id === requestId), bids: updatedRequest.bids.filter((bid) => bid._id !== bidId)} : request ))
+    navigate(`/requests`)
+  }
+
   if(!requests.length) { 
     return <main className="container">
               <div className='titleBar'>
